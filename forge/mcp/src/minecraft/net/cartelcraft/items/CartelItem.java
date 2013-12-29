018fb5fc130845cb6b5d9e39e2206ec6a27ee998
Cody Hafemeister <argixx@gmail.com>
2013-12-28 17:26:32 -0700

    First Upload
    Very First Upload of our mod

---
+++ forge/mcp/src/minecraft/net/cartelcraft/items/CartelItem.java
@@ -0,0 +1,19 @@
+package net.cartelcraft.items;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.cartelcraft.CartelCraft;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.item.Item;
+
+public class CartelItem extends Item {
+
+	public CartelItem(int par1) {
+		super(par1);
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister par1IconRegister) {
+		this.itemIcon = par1IconRegister.registerIcon(CartelCraft.modid + ":" + (this.getUnlocalizedName().substring(5)));
+	}
+}
---
+++ forge/mcp/src/minecraft/net/cartelcraft/items/ItemPseudoephedrine.java
@@ -0,0 +1,11 @@
+package net.cartelcraft.items;
+
+import net.minecraft.creativetab.CreativeTabs;
+
+public class ItemPseudoephedrine extends CartelItem {
+
+	public ItemPseudoephedrine(int par1) {
+		super(par1);
+		this.setCreativeTab(CreativeTabs.tabMisc);
+	}
+}
